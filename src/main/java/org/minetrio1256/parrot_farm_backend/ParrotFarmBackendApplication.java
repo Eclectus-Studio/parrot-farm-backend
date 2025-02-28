b6cecb9d63a00fe34a68593cbb536af5d75bcc53
Minetrio1256 <141367569+Minetrio1256@users.noreply.github.com>
2025-02-28 18:48:36 -0500

    First Commit

---
+++ src/main/java/org/minetrio1256/parrot_farm_backend/ParrotFarmBackendApplication.java
@@ -0,0 +1,13 @@
+package org.minetrio1256.parrot_farm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParrotFarmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParrotFarmBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/minetrio1256/parrot_farm_backend/ParrotFarmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.minetrio1256.parrot_farm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParrotFarmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
